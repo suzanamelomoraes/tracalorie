68e899e85e95ad1fce2006282771db021016be5a
Suzana-Melo-Moraes <suzanamelojor@gmail.com>
2020-04-06 15:32:02 +1200

    include a return to the Item controller that returns a function able to log the data structure, which is private to the module

--- app.js
+++ app.js
@@ -11,9 +11,19 @@ const ItemCtrl = (function(){
 
    //Data Structure - State
    const data = {
-       items: [],
+       items: [
+           {id:0, name: 'Steak Dinner', calories: 1200},
+           {id:1, name: 'Cookie', calories: 400},
+           {id:2, name: 'Eggs', calories: 300}
+       ],
        currentItem: null,
        totalCalories: 0
+   }
+
+   return {
+       logData: function(){
+           return data;
+       }
    }
 })();
 
