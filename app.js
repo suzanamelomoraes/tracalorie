8669a9cfd4369cc759c5451720eff47a171a7e02
Suzana-Melo-Moraes <suzanamelojor@gmail.com>
2020-04-15 15:20:09 +1200

    create a public method returned by storage controller that manages to get the items from the local storage and delete the item choosen by the user to be deleted updating the old data for the new one and return it to the local storage without the item deleted

--- app.js
+++ app.js
@@ -43,6 +43,16 @@ const StorageCtrl = (function(){
             });
             // Re set local storage
             localStorage.setItem('items', JSON.stringify(items));
+        },
+        deleteItemFromStorage: function(id){
+            let items = JSON.parse(localStorage.getItem('items'));
+
+            items.forEach(function(item, index){
+                if(id === item.id){
+                    items.splice(index, 1);
+                }
+            });
+            localStorage.setItem('items', JSON.stringify(items));
         }
     }
 })();
